de29741a627db73d948f13c89e3aff02a1176253
Dean <dean@deansantillan.com>
2022-01-17 21:28:33 +1000

    task(): add a graphql schema and queried it using graphiql.

--- server.js
+++ server.js
@@ -4,15 +4,76 @@ import { buildSchema } from 'graphql';
 import { graphqlHTTP } from 'express-graphql';
 const app = express();
 const schema = buildSchema(`
-type Query {
-    description: String
-    price: Float
-}
+	type Query {
+		products: [Product]
+		orders: [Order]
+	}
+
+	type Product {
+		id: ID!
+		description: String!
+		reviews: [Review]
+		price: Float!
+	}
+
+	type Review {
+		rating: Int!
+		comment: String
+	}
+
+	type Order {
+		date: String!
+		subtotal: Float!
+		items: [OrderItem]
+	}
+
+	type OrderItem {
+		 product: Product!
+		 quantity: Int!
+	}
 `);
 
 const root = {
-	description: 'Red Shoe',
-	price: 43.22,
+	products: [
+		{
+			id: 'redshoe',
+			description: 'Red Shoe',
+			price: 39.99,
+			reviews: [
+				{
+					rating: 5,
+					comment: 'absolutely beautiful shoes',
+				},
+			],
+		},
+		{
+			id: 'bluejean',
+			description: 'Blue Jean',
+			price: 59.99,
+		},
+	],
+	orders: [
+		{
+			date: '2022-01-10',
+			subtotal: 98.99,
+			items: [
+				{
+					product: {
+						id: 'redshoe',
+						description: 'old red shoe from long ago',
+						price: 49.99,
+						reviews: [
+							{
+								rating: 5,
+								comment: 'absolutely beautiful shoes',
+							},
+						],
+					},
+					quantity: 2,
+				},
+			],
+		},
+	],
 };
 
 app.use(express.json());
