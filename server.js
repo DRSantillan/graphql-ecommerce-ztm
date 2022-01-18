0838b718b54889fab59e5a19a28d7bc370057f20
Dean <dean@deansantillan.com>
2022-01-18 12:39:25 +1000

    task(): removed express graphql and replaced with apollo server. configured apollo to run with existing code.

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
 import express from 'express';
-import { graphqlHTTP } from 'express-graphql';
+import { ApolloServer } from 'apollo-server-express';
 import { makeExecutableSchema } from '@graphql-tools/schema';
 import { loadFilesSync } from '@graphql-tools/load-files';
 import path from 'path';
 import { allResolvers } from './resolvers/index.resolvers.js';
 
-
 const typesArray = await loadFilesSync(path.resolve('**/*.graphql'));
 const resolversArray = allResolvers;
-console.log(resolversArray)
-console.log(typesArray);
-const schema = makeExecutableSchema({
-	typeDefs: typesArray,
-	resolvers: resolversArray,
-});
-const app = express();
 
-app.use(express.json());
-app.use(
-	'/graphql',
-	graphqlHTTP({
-		schema: schema,
-		graphiql: true,
-	})
-);
+const startApolloServer = async () => {
+	const app = express();
 
-app.listen(3000, () => {
-	console.log('Running GraphQl server in express....');
-});
+	const schema = makeExecutableSchema({
+		typeDefs: typesArray,
+		resolvers: resolversArray,
+	});
+
+	const server = new ApolloServer({
+		schema,
+	});
+
+	await server.start();
+	server.applyMiddleware({ app, path: '/graphql' });
+
+	app.listen(3000, () => {
+		console.log('Running Apollo GraphQl server in express....');
+	});
+};
+
+startApolloServer();
